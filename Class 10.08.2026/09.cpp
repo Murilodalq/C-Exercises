38ab20c051d4be9af20c6dea1d2c810327175b66
Murilo Dalquano Santos <murilo.dalquano@gmail.com>
2026-08-10 19:51:05 -0300

    Fix Celsius conversion calculation and output

--- Class 10.08.2026/09.cpp
+++ Class 10.08.2026/09.cpp
@@ -4,7 +4,6 @@ main(){
 		float C = 0, F = 0;
 	printf("Digite o valor em Fahrenheit:\n");
 		scanf("%f", &F);
-	C = (F – 32);
-	C = C * (5/9);
-	printf("A temperatura em Celsius e: %.1f\n", F);
+	C = (F - 32.0) * (5.0 / 9.0);
+	printf("A temperatura em Celsius e: %.1f\n", C);
 }
